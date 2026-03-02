874b7fbf68e54c8b5899fd32790dd11a32a2499a
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-03-02 12:50:58 +0100

    added router + get response

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("OK");
+const express = require("express");
+const app = express();
+const port = 3000;
+const appUrl = `http://localhost:${port}/`;
+const postsRouter = require("./routers/posts");
+
+app.use("/posts", postsRouter);
+
+app.listen(port, () => {
+  console.log(`Server avviato su ${appUrl}`);
+});
