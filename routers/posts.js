78682bb9c0c029a58b593739663bae2d2786506a
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-03-02 13:13:09 +0100

    added all routes

--- routers/posts.js
+++ routers/posts.js
@@ -5,4 +5,24 @@ router.get("/", (req, res) => {
   res.send("Lista dei post");
 });
 
+router.get("/:id", (req, res) => {
+  res.send(`Dettaglio del post ${req.params.id}`);
+});
+
+router.post("/", (req, res) => {
+  res.send("Creazione nuovo post");
+});
+
+router.put("/:id", (req, res) => {
+  res.send(`Modifica integrale del post ${req.params.id}`);
+});
+
+router.patch("/:id", (req, res) => {
+  res.send(`Modifica parziale del post ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+  res.send(`Eliminazione del post ${req.params.id}`);
+});
+
 module.exports = router;
